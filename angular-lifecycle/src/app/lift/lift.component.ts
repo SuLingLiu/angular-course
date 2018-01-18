31b8056933ce618c1e0f242e237db6c64da67776
lsling <952543325@qq.com>
2018-01-18 14:50:19 +0800

    提交

--- angular-lifecycle/src/app/lift/lift.component.ts
+++ angular-lifecycle/src/app/lift/lift.component.ts
@@ -18,37 +18,43 @@ export class LiftComponent implements OnInit, OnChanges, DoCheck, AfterViewCheck
   logIte(msg: string) {
     console.log(`#${logIndex++} ${msg}`);
   }
+
+  //输入属性在这个方法里是没有被赋值的，如果要用到这个可以在ngOnInit里用
   constructor() {
     this.logIte('name属性在constructor里的值是：'+name);
+  }
+
+  //当一个父组件初始化一个或者改变一个子组件的输入属性的时候被调用，如果这个组件没有输入属性，则这个方法永远不会被调用，它的首次调用一定发生在ngOnInit之前
+  ngOnChanges(changes: SimpleChanges): void {
+    let name = changes['name'].currentValue;
+    this.logIte('name属性在ngOnChanges里的值是：'+name);
   }
 
   ngOnInit() {
     this.logIte('ngOnInit');
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    let name = changes['name'].currentValue;
-    this.logIte('name属性在ngOnChanges里的值是：'+name);
-  }
-
+  //用来检测的，在angular每个变更周期中调用的
   ngDoCheck(): void {
     this.logIte('ngDoCheck');
   }
 
-  ngAfterViewChecked(): void {
-    this.logIte('ngAfterViewChecked');
+  //下面两个是跟内容投影相关
+  ngAfterContentInit(): void {
+    this.logIte('ngAfterContentInit');
   }
 
   ngAfterContentChecked(): void {
     this.logIte('ngAfterContentChecked');
   }
 
+  //下面跟模板和检查相关
   ngAfterViewInit(): void {
     this.logIte('ngAfterViewInit');
   }
 
-  ngAfterContentInit(): void {
-    this.logIte('ngAfterContentInit');
+  ngAfterViewChecked(): void {
+    this.logIte('ngAfterViewChecked');
   }
 
   ngOnDestroy(): void {
